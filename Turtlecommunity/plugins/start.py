de78c75e35d1098af0a5ff4c23f791e93b71e4bf
NandyDark <58636468+nandydark@users.noreply.github.com>
2021-06-19 18:29:40 +0530

    Update start.py

--- Turtlecommunity/plugins/start.py
+++ Turtlecommunity/plugins/start.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.command(["start"]))
 async def start(_, message):
-    text= "Hey!! Thanks For Using This As Base For Your Bot\nVisit @turtlecommunitytg For Any Help"
+    text= "Hey!! Thanks For Using This As Base For Your Bot\nVisit @turtlecommunitytg For Any Help\n\nTry /stats"
     await message.reply_photo(
         caption=text,
         photo="https://telegra.ph/file/5a2e0f3c4e3268737c6b3.jpg")
