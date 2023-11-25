ffe39c462c87de865c9574a85c58894e5f2ab3a6
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-11-25 17:54:07 -0300

    @BubbalooTeam

--- Turtlecommunity/plugins/start.py
+++ Turtlecommunity/plugins/start.py
@@ -1,10 +1,50 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 
-@Client.on_message(filters.command(["start"]))
-async def start(_, message):
-    text= "Hey!! Thanks For Using This As Base For Your Bot"
-    await message.reply_photo(
-        caption=text,
-        photo="https://telegra.ph/file/5a2e0f3c4e3268737c6b3.jpg")
+HELPABLE: list[str] = []
+
+for modules in glob.glob("whiterkang/plugins/*.py"):
+    imported_module = import_module((modules)[:-3].replace("/", "."))
+    if hasattr(imported_module, "__help__"):
+        HELPABLE.append((modules.replace("/", ".")).split(".")[-2])
+
+
+@Client.on_callback_query(filters.regex(pattern=r"^start_back$"))
+@Client.on_message(filters.command("start"))
+async def start(c: Client, m: Message | CallbackQuery):
+    msg = ("<i>Olá <b>{}</b>!! Meu nome é <b>{}</b>. Estou aqui para divertir seu grupo</i>, <b>Fui feito com a biblioteca Pyrogram baseada na MTProto</b>").format(m.from_user.mention, c.me.first_name)
+    button = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="ℹ️ Sobre", callback_data="about"),
+                InlineKeyboardButton(
+                    text="📚 Comandos", callback_data="help_menu"),
+            ],
+        ]
+    )
+    if isinstance(m, Message):
+        await m.reply(text=msg, reply_markup=button)
+    else:
+        await c.edit_message_text(
+            chat_id=m.message.chat.id,
+            message_id=m.message.id,
+            text=msg,
+            reply_markup=button,
+        )
+
+@Client.on_callback_query(filters.regex(pattern=r"about"))
+async def about_menu(c: WhiterX, cb: CallbackQuery):
+    keyboard = [
+        [
+            InlineKeyboardButton(await tld(cb.message.chat.id, "BACK_BNT"), "start_back"),
+        ],
+    ]
+    text = ("<b>— yDixx</b>\n<b>Versão: <i>{}</i>").format("1.0.1")
+    await cb.edit_message_text(text, reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
